ce779b7b0a32fc21cb4302e50c30da2f22969ea6
MActor <action@github.com>
2020-02-06 11:04:01 +0000

    update by action 2020-02-06 19:03:29

--- public/area-stat-index.js
+++ public/area-stat-index.js
@@ -382,5 +382,6 @@ window.AreaStatIndex=[
   "getAreaStat.1580978034789.js",
   "getAreaStat.1580979855273.js",
   "getAreaStat.1580983437779.js",
-  "getAreaStat.1580985239939.js"
+  "getAreaStat.1580985239939.js",
+  "getAreaStat.1580987035966.js"
 ];
