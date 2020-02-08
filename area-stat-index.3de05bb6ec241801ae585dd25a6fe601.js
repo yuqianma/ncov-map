8d16a5358702b63b839cce8f1cd4407c23323ab7
yuqianma <yuqianma@users.noreply.github.com>
2020-02-08 06:05:32 +0000

    Automated deployment: Sat Feb  8 06:05:32 UTC 2020 db623a7dfa3926f739698ae16054390c69005c0f

--- area-stat-index.3de05bb6ec241801ae585dd25a6fe601.js
+++ area-stat-index.3de05bb6ec241801ae585dd25a6fe601.js
@@ -436,5 +436,6 @@ window.AreaStatIndex=[
   "getAreaStat.1581134641975.js",
   "getAreaStat.1581136447070.js",
   "getAreaStat.1581138262716.js",
-  "getAreaStat.1581140064040.js"
+  "getAreaStat.1581140064040.js",
+  "getAreaStat.1581141873749.js"
 ];
