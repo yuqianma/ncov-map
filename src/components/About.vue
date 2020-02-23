be83e4f6bd3c4a414d9b43cb674e3be5899de66c
yuqianma <mayq0422@gmail.com>
2020-02-24 00:09:40 +0800

    feat: render live readme

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,37 +1,22 @@
 <template>
-<pre>
-# ncov-map
-
-## 数据源说明
-
-仅供参考，一切以官方数据为准
-
-### 2020-01-24 之前
-
-https://mp.weixin.qq.com/s/JWxGVZieKj-S0uX9G42ybA
-
-整理结果：https://github.com/yuqianma/ncov-map/blob/master/public/before0124.csv
-
-### 2020-01-24 开始
-
-丁香园：https://ncov.dxy.cn/ncovh5/view/pneumonia
-
-## 每日数据汇总说明
-
-每半小时从丁香园抓取一次数据，保存在 https://github.com/yuqianma/ncov-map/tree/master/public/dxy
-
-往日数据：取24点前最后一次抓取结果
-当日数据：最新抓取结果
-
-## 参考
-
-公众号：向帆
-
-疫情的流图: https://mp.weixin.qq.com/s/Jroh5Us5QkwyJJI24z7uaA
-为有精度地观察疫情而设计: https://mp.weixin.qq.com/s/wsBxeePhy5i2YRqEInsfNQ
-
-向帆老师作品：
-http://zeelab.cn/WuhanCircleGraph/
-
-</pre>  
+<div v-html="compiledMarkdown">
+</div>
 </template>
+
+<script>
+const k = Date.now();
+export default {
+  data: () => ({
+    readme: "#waiting..."
+  }),
+  created() {
+    fetch('https://cdn.jsdelivr.net/gh/yuqianma/ncov-map@master/README.md?_=' + k)
+    .then(res => res.text()).then(t => this.readme = t);
+  },
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.readme);
+    }
+  },
+}
+</script>
